001e895598da955312d25067052dab041875192e
Rishav Thakur <rishav.21m@gmail.com>
2021-11-07 15:06:04 +0530

    inital commit

--- index.js
+++ index.js
@@ -137,16 +137,21 @@ const playSick = () => {
   const tempo = 120; // BPM (beats per minute)
   const eighthNoteTime = (60 / tempo) / 2;
 
+  const kick = new Kick(context);
+  const snare = new Snare(context);
+
   for (var bar = 0; bar < 3; bar++) {
     var time = startTime + bar * 8 * eighthNoteTime;
 
     // Play the bass (kick) drum on beats 1, 5
-    playSound(bufferObj.kick, time);
-    playSound(bufferObj.kick, time + 3 * eighthNoteTime);
-    playSound(bufferObj.kick, time + 5 * eighthNoteTime);
+
+    kick.trigger(time);
+    kick.trigger(time + 3 * eighthNoteTime);
+    kick.trigger(time + 5 * eighthNoteTime);
     // Play the snare drum on beats 3, 7
-    playSound(bufferObj.snare, time + 2 * eighthNoteTime);
-    playSound(bufferObj.snare, time + 6 * eighthNoteTime);
+    
+    snare.trigger(time + 2 * eighthNoteTime);
+    snare.trigger(time + 6 * eighthNoteTime);
   }
 }
 
@@ -166,3 +171,82 @@ function AnimateButton() {
   console.log(frequencyData);
 
 } 
+function Kick(context) {
+	this.context = context;
+};
+
+Kick.prototype.setup = function() {
+	this.osc = this.context.createOscillator();
+	this.gain = this.context.createGain();
+	this.osc.connect(this.gain);
+	this.gain.connect(this.context.destination)
+};
+
+Kick.prototype.trigger = function(time) {
+	this.setup();
+
+	this.osc.frequency.setValueAtTime(150, time);
+	this.gain.gain.setValueAtTime(1, time);
+
+	this.osc.frequency.exponentialRampToValueAtTime(0.01, time + 0.5);
+	this.gain.gain.exponentialRampToValueAtTime(0.01, time + 0.5);
+
+	this.osc.start(time);
+
+	this.osc.stop(time + 0.5);
+}
+
+
+function Snare(context) {
+  this.context = context;
+};
+
+Snare.prototype.setup = function() {
+  this.noise = this.context.createBufferSource();
+  this.noise.buffer = this.noiseBuffer();
+
+  var noiseFilter = this.context.createBiquadFilter();
+  noiseFilter.type = 'highpass';
+  noiseFilter.frequency.value = 1000;
+  this.noise.connect(noiseFilter);
+
+  this.noiseEnvelope = this.context.createGain();
+  noiseFilter.connect(this.noiseEnvelope);
+
+  this.noiseEnvelope.connect(this.context.destination);
+
+  this.osc = this.context.createOscillator();
+  this.osc.type = 'triangle';
+
+  this.oscEnvelope = this.context.createGain();
+  this.osc.connect(this.oscEnvelope);
+  this.oscEnvelope.connect(this.context.destination);
+};
+
+Snare.prototype.noiseBuffer = function() {
+  var bufferSize = this.context.sampleRate;
+  var buffer = this.context.createBuffer(1, bufferSize, this.context.sampleRate);
+  var output = buffer.getChannelData(0);
+
+  for (var i = 0; i < bufferSize; i++) {
+    output[i] = Math.random() * 2 - 1;
+  }
+
+  return buffer;
+};
+
+Snare.prototype.trigger = function(time) {
+  this.setup();
+
+  this.noiseEnvelope.gain.setValueAtTime(1, time);
+  this.noiseEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.2);
+  this.noise.start(time)
+
+  this.osc.frequency.setValueAtTime(100, time);
+  this.oscEnvelope.gain.setValueAtTime(0.7, time);
+  this.oscEnvelope.gain.exponentialRampToValueAtTime(0.01, time + 0.1);
+  this.osc.start(time)
+
+  this.osc.stop(time + 0.2);
+  this.noise.stop(time + 0.2);
+};
